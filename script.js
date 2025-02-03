6f5fa1d3a7a8dcffd7bc56efaff99b07639abfc2
austineg <148214756+austineg@users.noreply.github.com>
2025-02-03 12:38:43 -0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll(".nav-item a");
-
-function enlargeImage() {
-    document.getElementById("nav-item a").style.color = "#40E0D0 ";
+document.querySelector(".nav-item a").style.color = "#40E0D0";
+function enlargeImage(link) {
+   link.style.color = "#40E0D0"; // Turquoise color
   }
 
 links.forEach(link => {
@@ -10,9 +10,10 @@ links.forEach(link => {
     // links.forEach(l => l.classList.remove("active"));
     for (let i = 0; i < links.length; i++) {
       links[i].classList.remove("active");
+      links[i].style.color = ""; // Reset color
     }
     // Add the active class to the clicked link
     this.classList.add("active");
-    enlargeImage();
+    enlargeImage(this);
   });
 });
